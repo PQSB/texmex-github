f00ff8800ab22a8f7816e2949b161a8b1968860e
agentez <a.galea.2022@alumnos.urjc.es>
2023-04-28 15:25:40 +0200

    Añadir imprime_recetas.py y recteas.md

---
+++ imprime_receta.py
@@ -0,0 +1,4 @@
+# Abrir el fichero "recetas.md"
+archivo = open("recetas.md")
+# Imprimir el contenido del fichero
+print(archivo.read())
---
+++ recetas.md
@@ -0,0 +1,1 @@
+El contenido que prefiera ahora modificadito.
